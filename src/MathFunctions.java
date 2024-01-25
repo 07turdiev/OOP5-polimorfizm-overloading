a9a6dabfa6f236a8b6259b791ac019b4e927a981
Elbek <elbekturdiyev20020707@gmail.com>
2024-01-26 01:09:16 +0500

    Initial commit

---
+++ src/MathFunctions.java
@@ -0,0 +1,188 @@
+public class MathFunctions {
+    public static byte add(byte a, byte b){
+        return (byte) (a+b);
+    }
+
+    public static short add(short a, short b){
+        return (short) (a+b);
+    }
+
+    public static int add(int a, int b){
+        return a+b;
+    }
+
+    public static long add(long a, long b){
+        return a+b;
+    }
+
+    public static float add(float a, float b){
+        return a+b;
+    }
+
+    public static double add(double a, double b){
+        return a+b;
+    }
+
+    public static double add(int a, double b){
+        return a+b;
+    }
+
+    public static double add(double a, int b){
+        return a+b;
+    }
+//////////////////////////  AYITMASI  /////////////////////////////////////////////
+
+    public static byte sub(byte a, byte b){
+        return (byte) (a-b);
+    }
+
+    public static short sub(short a, short b){
+        return (short) (a-b);
+    }
+
+    public static int sub(int a, int b){
+        return a-b;
+    }
+
+    public static long sub(long a, long b){
+        return a-b;
+    }
+
+    public static float sub(float a, float b){
+        return a-b;
+    }
+
+    public static double sub(double a, double b){
+        return a-b;
+    }
+
+    public static double sub(int a, double b){
+        return a-b;
+    }
+
+    public static double sub(double a, int b){
+        return a-b;
+    }
+
+
+    ////////////////////  KOPAYTMA   //////////////////////////////////
+
+    public static byte multiply(byte a, byte b){
+        return (byte) (a*b);
+    }
+
+    public static short multiply(short a, short b){
+        return (short) (a*b);
+    }
+
+    public static int multiply(int a, int b){
+        return a*b;
+    }
+
+    public static long multiply(long a, long b){
+        return a*b;
+    }
+
+    public static float multiply(float a, float b){
+        return a*b;
+    }
+
+    public static double multiply(double a, double b){
+        return a*b;
+    }
+
+    public static double multiply(int a, double b){
+        return a*b;
+    }
+
+    public static double multiply(double a, int b){
+        return a*b;
+    }
+
+
+    ////////////////////  BO'LINMA  ///////////////////////////////////////////////
+
+    public static double div(byte a, byte b){
+        return (byte) (a*b);
+    }
+
+    public static double div(short a, short b){
+        return (short) (a*b);
+    }
+
+    public static double div(int a, int b){
+        return (double) a /b;
+    }
+
+    public static double div(long a, long b){
+        return (double) a /b;
+    }
+
+    public static double div(float a, float b){
+        return a/b;
+    }
+
+    public static double div(double a, double b){
+        return a/b;
+    }
+
+    public static double div(int a, double b){
+        return a/b;
+    }
+
+    public static double div(double a, int b){
+        return a/b;
+    }
+
+    ////////////////////  ABC  //////////////////////
+
+    public static byte abs(byte a){
+        return (byte) Math.abs(a);
+    }
+
+    public static short abs(short a){
+        return (short) Math.abs(a);
+    }
+
+    public static int abs(int a){
+        return Math.abs(a);
+    }
+
+    public static long abs(long a){
+        return Math.abs(a);
+    }
+
+    public static float abs(float a){
+        return Math.abs(a);
+    }
+
+    public static double abs(double a){
+        return Math.abs(a);
+    }
+
+    ////////////////  POW  //////////////////////////////////
+
+    public static byte pow(byte a){
+        return (byte) Math.pow(a, 2);
+    }
+
+    public static short pow(short a){
+        return (short) Math.pow(a, 2);
+    }
+
+    public static int pow(int a){
+        return (byte) Math.pow(a, 2);
+    }
+
+    public static long pow(long a){
+        return (byte) Math.pow(a, 2);
+    }
+
+    public static float pow(float a){
+        return (byte) Math.pow(a, 2);
+    }
+
+    public static double pow(double a){
+        return (byte) Math.pow(a, 2);
+    }
+}
